a19f0218e6d5b1d48db49d998905cc4ce4688490
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-03-27 11:55:39 +0300

    control lab

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ function scuberGreetingForFeet(ride) {
   else if (ride > 2000 && ride <= 2500) {
     return "I will gladly take your thirty bucks.";
   }
-  else if (ride>2500) {
+  else if (ride > 2500) {
     return "No can do.";
   }
-  
 }
 
 
@@ -28,14 +27,14 @@ function ternaryCheckCity(city) {
 
 function switchOnCharmFromTip(tip) {
   // Write your code here!
-  switch(tip) {
-  case "generous":
-  return "Thank you so much."
-  break
-  case "not as generous":
-  return "Thank you."
-  break;
-  default:
-  return "Bye."
-}
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+      break
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+  }
 }
